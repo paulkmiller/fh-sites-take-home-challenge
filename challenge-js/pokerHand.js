838d1d9aad9ee87967208677739fbcde9a6d5332
Paul Miller <pmillerk88@gmail.com>
2023-04-20 13:50:27 -0600

    Too deep in the rabbit hole; reverting

--- challenge-js/pokerHand.js
+++ challenge-js/pokerHand.js
@@ -27,50 +27,30 @@ class PokerHand {
       sortedValues.push({ value, count: values[value] });
     }
 
-    // sort sortedValues by count ascending
+    console.log(sortedValues);
+
+    // sort sortedValues by count descending
     sortedValues.sort((a, b) => {
-      const rankA = 'A23456789TJQK'.indexOf(a.value);
-      const rankB = 'A23456789TJQK'.indexOf(b.value);
-      
-      return rankA - rankB;
+      const rankA = '23456789TJQKA'.indexOf(a.value);
+      const rankB = '23456789TJQKA'.indexOf(b.value);
+
+      return rankB - rankA;
     });
-    
+
     // check if the hand is a flush (all cards have the same suit)
     const isFlush = Object.keys(suits).length === 1;
-    
+
     // check if the hand is a straight (all cards are consecutive values)
-
-    // initial attempt
-    // const isStraight =
-    //   sortedValues[0].count === 1 &&
-    //   sortedValues[0].value === 'A' &&
-    //   sortedValues[1].value === '5';
-    
-    // refactor attempt
-    // const isStraight = sortedValues.every((card, i, arr) => {
-    //   if (i === 0) return true;
-    //   const prevRank = 'A23456789TJQK'.indexOf(arr[i - 1].value);
-    //   const currantRank = 'A23456789TJQK'.indexOf(card.value);
-    //   return currantRank === prevRank + 1 || (i === 4 && prevRank === 11 && currantRank === 0);
-    // });
-
-    // 2nd refactor atttempt
-    function isStraight(sortedValues) {
-      const ranks = 'A23456789TJQK';
-      const handString = sortedValues.map((item) => item.value).join('');
-    
-      const isConsecutive = ranks.includes(handString);
-      const isLowAceStraight = handString === '2345A';
-
-      const allUnique = sortedValues.every((card) => card.count === 1);
-    
-      return allUnique && (isConsecutive || isLowAceStraight);
-    }
+    //
+    const isStraight =
+      sortedValues[0].count === 1 &&
+      sortedValues[0].value === 'A' &&
+      sortedValues[1].value === '5';
 
     // determine the rank of the hand based on flush and straight conditions + group counts
     if (isFlush && isStraight && sortedValues[0].value === 'A') {
-      // returns straight flush instead of royal flush with the following hand: As Ks Qs Js 10s
-      // maybe an issue with the isStraight function?
+      // returns flush instead of royal flush with the following hand: As Ks Qs Js 10s
+      // console.log shows the values are not arranged properly; 10 A K Q J instead of A K Q J 10
       return 'Royal Flush';
     } else if (isFlush && isStraight) {
       return 'Straight Flush';
