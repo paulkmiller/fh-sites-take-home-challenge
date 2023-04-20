fe9c302769109527be2f42557e92ef806cfeba80
Paul Miller <pmillerk88@gmail.com>
2023-04-20 11:21:11 -0600

    WE'RE EVALUATIN BABY-- tests need work

--- challenge-js/pokerHand.js
+++ challenge-js/pokerHand.js
@@ -36,32 +36,35 @@ class PokerHand {
     });
 
     // check if the hand is a flush (all cards have the same suit)
-
+    const isFlush = Object.keys(suits).length === 1;
+    
     // check if the hand is a straight (all cards are consecutive values)
+    const isStraight = sortedValues[0].count === 1 &&
+                       sortedValues[0].value === 'A' &&
+                       sortedValues[1].value === '5';
 
     // determine the rank of the hand based on flush and straight conditions + group counts
-    // if isFlush and isStraight and the first value is 'A'
-      // return 'Royal Flush'
-    // else if isFlush and isStraight
-      // return 'Straight Flush'
-    // else if isFlush
-      // return 'Flush'
-    // else if isStraight
-      // return 'Straight'
-    // else if sortedValues[0].count is 4
-      // return 'Four of a Kind'
-    // else if sortedValues[0].count is 3 and sortedValues[1].count is 2
-      // return 'Full House'
-    // else if sortedValues[0].count is 3
-      // return 'Three of a Kind'
-    // else if sortedValues[0].count is 2 and sortedValues[1].count is 2
-      // return 'Two Pair'
-    // else if sortedValues[0].count is 2
-      // return 'One Pair'
-    // else
-      // return 'High Card'
-
-    return 'Royal Flush';
+    if (isFlush && isStraight && sortedValues[0].value === 'A') {
+      return 'Royal Flush';
+    } else if (isFlush && isStraight) {
+      return 'Straight Flush';
+    } else if (isFlush) {
+      return 'Flush';
+    } else if (isStraight) {
+      return 'Straight';
+    } else if (sortedValues[0].count === 4) {
+      return 'Four of a Kind';
+    } else if (sortedValues[0].count === 3 && sortedValues[1].count === 2) {
+      return 'Full House';
+    } else if (sortedValues[0].count === 3) {
+      return 'Three of a Kind';
+    } else if (sortedValues[0].count === 2 && sortedValues[1].count === 2) {
+      return 'Two Pair';
+    } else if (sortedValues[0].count === 2) {
+      return 'One Pair';
+    } else {
+      return 'High Card';
+    }
   }
 }
 
